15348a913ea121b89886f8a7199d8f907394e6f9
surenkutty <surenkutty2004@gmail.com>
2025-05-06 14:40:59 +0530

    fix error

--- foods/admin.py
+++ foods/admin.py
@@ -1,37 +1,29 @@
 from django.contrib import admin
 from .models import Category, Food, FoodLog, Routine
-from unfold.admin import ModelAdmin
-from unfold.contrib.filters.admin import ChoicesDropdownFilter, RelatedDropdownFilter, RangeDateFilter
 
 @admin.register(Category)
-class CategoryAdmin(ModelAdmin):
-    list_display = ['id', 'name', 'slug']
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
     prepopulated_fields = {'slug': ('name',)}
-    search_fields = ['name']
-
+    search_fields = ('name',)
 
 @admin.register(Food)
-class FoodAdmin(ModelAdmin):
-    list_display = ['id', 'name', 'category', 'calories']
-    list_filter = [('category', RelatedDropdownFilter)]
-    search_fields = ['name']
-
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'calories', 'protein', 'carbs', 'fats')
+    list_filter = ('category',)
+    search_fields = ('name',)
+    autocomplete_fields = ('category',)
 
 @admin.register(FoodLog)
-class FoodLogAdmin(ModelAdmin):
-    list_display = ['id', 'user', 'food', 'meal_type', 'date', 'quantity']
-    list_filter = [
-        ('meal_type', ChoicesDropdownFilter),
-        ('date', RangeDateFilter)
-    ]
-    search_fields = ['user__username', 'food__name']
-
+class FoodLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'food', 'meal_type', 'date', 'quantity')
+    list_filter = ('meal_type', 'date')
+    search_fields = ('user__username', 'food__name')
+    autocomplete_fields = ('user', 'food')
 
 @admin.register(Routine)
-class RoutineAdmin(ModelAdmin):
-    list_display = ['id', 'user', 'title', 'is_completed', 'date']
-    list_filter = [
-        ('is_completed', ChoicesDropdownFilter),
-        ('date', RangeDateFilter)
-    ]
-    search_fields = ['user__username', 'title']
+class RoutineAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'date', 'is_completed')
+    list_filter = ('is_completed', 'date')
+    search_fields = ('user__username', 'title')
+    autocomplete_fields = ('user',)
